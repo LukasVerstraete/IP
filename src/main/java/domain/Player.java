1c15762efc03185622dd63a04089f45661c05a6c
BuildTools <unconfigured@null.spigotmc.org>
2016-03-31 01:55:00 +0200

    Final version framework project

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,7 +1,9 @@
 package domain;
 
 import java.io.Serializable;
+import javax.annotation.Generated;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import util.DomainException;
 
---
+++ src/main/java/ui/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+import domain.Player;
+import services.player.PlayerService;
+import util.MemoryLocation;
+
+/**
+ *
+ * @author Lukas
+ */
+public class Main {
+    
+//    public static void main(String [] args)
+//    {
+//        PlayerService service = new PlayerService(MemoryLocation.REMOTE);
+//        Player player = new Player("Lukas", "Lukas", "Verstraete");
+//        service.add(player);
+//        Player p= service.get(player.getUsername());
+//        System.out.println(p);
+//    }
+    
+}
